7e0ceb0550bdb3e93313a61aacb2ad0441efc379
HermanJasser <herjas00989@stud.noroff.no>
2024-09-26 14:01:22 +0200

    fikset et problem i authguard

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -13,3 +13,11 @@ logOutBtn.addEventListener("click", onLogout);
 
 //this is going to list 12 posts on the home page
 readPosts();
+
+function checkIfLoggedIn(){
+    if(localStorage.token){
+        document.getElementById("log-in-register-btn").style.opacity = 0;
+     }
+}
+
+checkIfLoggedIn();
--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -2,11 +2,5 @@ export function authGuard() {
   if (!localStorage.token) {
     alert("You must be logged in to view this page");
     window.location.href = "/auth/login/";
-  } else if(localStorage.token){
-   // document.getElementById("log-in-register-btn").style.display = "none";
-
   }
-
 }
-
-
